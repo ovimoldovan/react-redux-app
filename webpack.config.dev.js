3225aed510cb8c0879a476b013249321f6d15779
Ovidiu Moldovan <ovi1602@gmail.com>
2020-10-24 17:34:06 +0300

    npm start build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = 'development';
 module.exports = {
     mode: 'development',
     target: 'web',
-    devTool: 'cheap-module-source-map',
+    devtool: 'cheap-module-source-map',
     entry: './src/index',
     output: {
         path: path.resolve(__dirname, "build"),
@@ -16,8 +16,8 @@ module.exports = {
     },
     devServer: {
         stats: 'minimal',
-        overlay: 'true',
-        historyApiFallback: 'true',
+        overlay: true,
+        historyApiFallback: true,
         disableHostCheck: true,
         headers: {"Access-Control-Allow-Origin": "*"},
         https: false
